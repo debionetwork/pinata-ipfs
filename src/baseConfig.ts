06517c1cdf7595213411eea92e13de4df347bd68
Dicki <bukandicki@gmail.com>
2022-02-25 10:57:15 +0700

    feat: Code improvement and add more function

--- src/baseConfig.ts
+++ src/baseConfig.ts
@@ -1,2 +1,3 @@
-export const baseUrl = 'https://api.pinata.cloud';
-export const pinFileToIpfsEndpoint = '/pinning/pinFileToIPFS';
+export const gateway = "https://ipfs.debio.network/ipfs/"
+export const baseUrl = "https://api.pinata.cloud"
+export const pinFileToIpfsEndpoint = "/pinning/pinFileToIPFS"
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,78 @@
-import axios from 'axios';
-import NodeFormData from 'form-data';
-import { baseUrl, pinFileToIpfsEndpoint } from './baseConfig';
+import { baseUrl, gateway, pinFileToIpfsEndpoint } from "./baseConfig"
+import axios, { AxiosRequestHeaders } from "axios"
+import Kilt from "@kiltprotocol/sdk-js"
+import NodeFormData from "form-data"
 
-export function uploadFile(pinataApiOptions: any, file: Blob, jwt: string) {
+interface IPair {
+  publicKey: string,
+  secretKey: string
+}
+
+export const uploadFile = (pinataApiOptions: any, file: Blob, headers: AxiosRequestHeaders) => {
   return new Promise((resolve, reject) => {
-    const data: any = new NodeFormData();
-    data.append('file', file);
+    const data = new NodeFormData()
+    const endpoint = `${baseUrl}${pinFileToIpfsEndpoint}`
 
-    const endpoint = `${baseUrl}${pinFileToIpfsEndpoint}`;
+    data.append("file", file)
 
-    if (pinataApiOptions) {
-      if (pinataApiOptions.pinataMetadata) {
-        data.append('pinataMetadata', JSON.stringify(pinataApiOptions.pinataMetadata));
-      }
-      if (pinataApiOptions.pinataOptions) {
-        data.append('pinataOptions', JSON.stringify(pinataApiOptions.pinataOptions));
-      }
-    }
+    if (pinataApiOptions.pinataMetadata) data.append("pinataMetadata", JSON.stringify(pinataApiOptions.pinataMetadata))
+    if (pinataApiOptions.pinataOptions) data.append("pinataOptions", JSON.stringify(pinataApiOptions.pinataOptions))
 
-    axios
-      .post(endpoint, data, {
-        withCredentials: true,
-        maxContentLength: 'Infinity' as any,
-        maxBodyLength: 'Infinity' as any,
-        headers: {
-          'Content-type': `multipart/form-data; boundary= ${data._boundary}`,
-          authorization: `Bearer ${jwt}`,
-        },
-      })
-      .then((result) => {
-        if (result.status !== 200) {
-          reject(new Error(`Unknown server response while pinning File to IPFS: ${result}`));
-        }
-        resolve(result.data);
-      })
-      .catch((error) => reject(error));
-  });
+    axios.post(endpoint, data, {
+      withCredentials: true,
+      maxContentLength: "Infinity" as any, // NOTE: Allow axios to upload large file
+      maxBodyLength: "Infinity" as any,
+      headers
+    }).then(result => {
+      if (result.status !== 200) reject(new Error(`unknown server response while pinning File to IPFS: ${result}`))
+      else resolve(result.data)
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+
+export const getFileUrl = (cid: string) => {
+  return `${gateway}${cid}`
+}
+
+export const downloadFile = async (ipfsLink: string) => {
+  console.log("Downloading...")
+
+  const response = await fetch(ipfsLink)
+  const data = await response.json()
+
+  console.log("Success Downloaded!")
+
+  return data
+}
+
+export const decryptFile = (obj: any, pair: IPair, type: string) => {
+  const box: any = Object.values(obj[0].data.box)
+  const nonce: any = Object.values(obj[0].data.nonce)
+  let decryptedFile: any
+
+  const toDecrypt = {
+    box: Uint8Array.from(box),
+    nonce: Uint8Array.from(nonce)
+  }
+
+  if (type === "application/pdf") decryptedFile = Kilt.Utils.Crypto.decryptAsymmetric(toDecrypt, pair.publicKey, pair.secretKey)
+  else decryptedFile = Kilt.Utils.Crypto.decryptAsymmetricAsStr(toDecrypt, pair.publicKey, pair.secretKey)
+
+  if (!decryptedFile) console.log("Undefined File", decryptedFile)
+  else return decryptedFile
+}
+
+export const downloadDocumentFile = (data: any, fileName: string, type: string) => {
+  try {
+    const blob = new Blob([data], { type })
+    const a = document.createElement("a")
+
+    a.download = fileName
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ["text/json", a.download, a.href].join(":")
+  } catch (error) {
+    console.error(error)
+  }
 }
